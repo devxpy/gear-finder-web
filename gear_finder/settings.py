697bf6c0732d6c402d75620822b100e52aca6dce
Dev Aggarwal <devxpy@gmail.com>
2019-07-24 18:02:10 +0530

    deployment

--- gear_finder/settings.py
+++ gear_finder/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = f'/{config("STATIC_ROOT")}/'
-STATIC_ROOT = config("STATIC_ROOT")
+STATIC_URL = f'/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = "user.CustomUser"
