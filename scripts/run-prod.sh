5c0920765120f9df7b8e586e42eff3e46e59d7c8
Dev Aggarwal <devxpy@gmail.com>
2020-01-23 02:14:55 +0530

    tweak scripts

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -2,9 +2,15 @@
 
 set -ex
 
-./scripts/pre-deploy.sh
+wget -qO deps.tar.bz2 $DEPS_TARBALL
+tar -xjf deps.tar.bz2 .
 
+export PYTHONPATH=$PWD:$PYTHONPATH
+export PATH=$PWD/bin:$PATH
+
+python setup.py build_ext --inplace
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+nohup ./caddy -conf scripts/Caddyfile &
 gunicorn gear_finder.wsgi --bind unix:/usr/src/app/gunicorn.sock
